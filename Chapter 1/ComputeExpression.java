f29d39b8523bca55e7d72807b1cfaacc0517674f
jordanruesch <60372617+jordanruesch@users.noreply.github.com>
2020-01-29 18:37:54 -0700

    Add files via upload

---
+++ Chapter 1/ComputeExpression.java
@@ -0,0 +1,10 @@
+/*
+Author: Jordan Ruesch
+Date: 1/29/2020
+Excercise 1-5 to compute the value of an expression.
+*/
+class ComputeExpression {
+	public static void main(String[] args) {
+		System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));
+	}
+}
