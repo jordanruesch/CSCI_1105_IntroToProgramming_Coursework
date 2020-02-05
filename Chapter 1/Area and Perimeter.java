14a78efe34520d6c16aada739b9d13b89e235cc1
jordanruesch <60372617+jordanruesch@users.noreply.github.com>
2020-02-04 18:12:04 -0700

    Add files via upload

---
+++ Chapter 1/Area and Perimeter.java
@@ -0,0 +1,11 @@
+/*
+Author: Jordan Ruesch
+Date: 2/4/2020
+Exercise 1-9 to compute area and perimeter.
+*/
+class ComputerExpression {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println(2*(4.5 + 7.9));
+	}
+}
