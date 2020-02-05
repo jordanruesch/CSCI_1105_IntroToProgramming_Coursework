4eec9f595bde7f1e5ae24227265ed96348e9c36f
jordanruesch <60372617+jordanruesch@users.noreply.github.com>
2020-02-04 19:10:12 -0700

    Update and rename Area and Perimeter.java to Area Perimeter.java

--- Chapter 1/Area Perimeter.java
+++ Chapter 1/Area Perimeter.java
@@ -3,7 +3,7 @@ Author: Jordan Ruesch
 Date: 2/4/2020
 Exercise 1-9 to compute area and perimeter.
 */
-class ComputerExpression {
+class AreaPerimeter {
 	public static void main(String[] args) {
 		System.out.println(4.5 * 7.9);
 		System.out.println(2*(4.5 + 7.9));
