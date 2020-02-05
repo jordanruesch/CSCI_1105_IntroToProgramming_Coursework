e806c8248bcafb01796d99796fff5b7b252bfea5
jordanruesch <60372617+jordanruesch@users.noreply.github.com>
2020-02-04 19:08:53 -0700

    Update and rename Find X and Y.java to Find XandY.java

--- Chapter 1/Find XandY.java
+++ Chapter 1/Find XandY.java
@@ -3,7 +3,7 @@ Author: Jordan Ruesch
 Date: 2/4/2020
 Exercise 1-13 to find the value of x and y.
 */
-class ComputeExpression {
+class FindXandY {
 	public static void main(String[] args) {
 		System.out.println((44.5 * .55 - 50.2 * 5.9)/(3.4 * .55 - 50.2 * 2.1));
 		System.out.println((3.4 * 5.9 - 44.5 * 2.1)/(3.4 * .55 - 50.2 * 2.1));
